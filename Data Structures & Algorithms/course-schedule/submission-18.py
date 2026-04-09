9b8918eafc6b7e6582b19513ef3c7624d0da90a3
Rajas Chitale <56029517+rajas1310@users.noreply.github.com>
2026-04-08 20:24:25 -0700

    Add: course-schedule - submission-18

---
+++ Data Structures & Algorithms/course-schedule/submission-18.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        graph = defaultdict(list)
+        for c, pre in prerequisites:
+            graph[c].append(pre)
+
+        path = set()
+
+        def dfs(cnum):
+            if cnum in path:
+                return False
+            
+            if graph[cnum] == []:
+                return True
+            
+            path.add(cnum)
+            for pre in graph[cnum]:
+                if not dfs(pre):
+                    return False
+            path.remove(cnum)
+            # graph[cnum] = []
+            return True
+            
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True 
