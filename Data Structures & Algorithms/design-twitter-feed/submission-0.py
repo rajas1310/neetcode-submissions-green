c93cc5c375192875c69923debbc2a25016754704
Rajas Chitale <56029517+rajas1310@users.noreply.github.com>
2026-04-24 09:17:17 -0700

    Add: design-twitter-feed - submission-0

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.py
@@ -0,0 +1,26 @@
+class Twitter:
+
+    def __init__(self):
+        self.KfollowsV_graph = defaultdict(set)
+        self.tweet_order = []
+        self.user2tweets = defaultdict(list)
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.user2tweets[userId].append(tweetId)
+        self.tweet_order.append((userId, tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        visible_users = self.KfollowsV_graph[userId] | {userId}
+        res = []
+        for uid, tid in reversed(self.tweet_order):
+            if uid in visible_users:
+                res.append(tid)
+                if len(res) == 10: break
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.KfollowsV_graph[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.KfollowsV_graph[followerId].discard(followeeId)
